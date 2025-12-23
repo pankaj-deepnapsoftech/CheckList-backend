f8731b6bbeed81a52a6dd7fbf3244ea8fe088f6f
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-12-23 14:33:39 +0530

    Some change notification

--- src/models/notification.modal.js
+++ src/models/notification.modal.js
@@ -1,15 +1,17 @@
 
-import {Schema,model} from "mongoose";
+import { Schema, model } from "mongoose";
 
-const notificationSchema = new Schema ({
-    title:{type:String,required:true},
-    description:{type:String},
-    reciverId:{type:Schema.Types.ObjectId,ref:"User",required:true},
-    senderId:{type:Schema.Types.ObjectId,ref:"User"},
-    status:{type:String,required:true,default:"send",enum:["send",'recived','view']}
+const notificationSchema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String },
+    reciverId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    senderId: { type: Schema.Types.ObjectId, ref: "User" },
+    status: { type: String, required: true, default: "send", enum: ["send", 'recived', 'view'] },
+    assembly: { type: Schema.Types.ObjectId, ref: "Assembly", required: true },
+    process_id: { type: Schema.Types.ObjectId, ref: "Process", required: true },
 });
 
-export const NotificationModal = model("Notification",notificationSchema);
+export const NotificationModal = model("Notification", notificationSchema);
 
 
 
--- src/services/notification.service.js
+++ src/services/notification.service.js
@@ -7,7 +7,11 @@ export const CreateNotification = async (data) => {
 };
 
 export const GetNotification = async (user,skip,limit) => {
-    const result = await NotificationModal.find({reciverId:user}).sort({_id:-1}).skip(skip).limit(limit).populate({path:"senderId",select:"desigination user_id email full_name"}).lean();
+    const result = await NotificationModal.find({reciverId:user}).sort({_id:-1}).skip(skip).limit(limit).populate([
+        {path:"senderId",select:"desigination user_id email full_name"},
+        {path:"assembly",select:"assembly_name assembly_number"},
+        {path:"process_id",selct:"process_name process_no"}
+    ]).lean();
     return result;
 };
 
@@ -18,4 +22,5 @@ export const UpdateNotification = async(id,data) => {
 
 export const GetUpdateAll = async () => {
     const result = await NotificationModal.updateMany();
-}
+    return result;
+};
